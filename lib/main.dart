d3a92df26237eeb9c8294b67871887b0ea55be91
tamathecxder <yudistiraeka.pratama012@gmail.com>
2022-10-05 16:02:32 +0700

    HTTP Get pada flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -16,8 +18,26 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late String id;
+  late String name;
+  late String email;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    id = "";
+    name = "";
+    email = "";
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,12 +50,43 @@ class HomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("Data"),
+            Text(
+              "ID: $id",
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              "Name: $name",
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              "Email: $email",
+              style: TextStyle(fontSize: 20),
+            ),
             SizedBox(
               height: 14,
             ),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () async {
+                var myResponse = await http.get(
+                  Uri.parse("https://reqres.in/api/users/1"),
+                );
+
+                if (myResponse.statusCode == 200) {
+                  Map<String, dynamic> data =
+                      json.decode(myResponse.body) as Map<String, dynamic>;
+                  print("Successfull");
+                  print(data);
+
+                  setState(() {
+                    id = data["data"]["id"].toString();
+                    email = data["data"]["email"].toString();
+                    name =
+                        "${data['data']['first_name']} ${data['data']['last_name']}";
+                  });
+                } else {
+                  print("Error: ${myResponse.statusCode}");
+                }
+              },
               child: Text("Get Data"),
             ),
           ],
